d3f3e76af9d2f187e13d7b2c1b69bc68d373806d
daviddiazh <daviddiazlds1221@gmail.com>
2024-11-06 17:36:17 -0500

    feat: desk navbar ok

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import Link from 'next/link'
+import React from 'react'
+import styles from './styles.module.css'
+import { usePathname } from 'next/navigation'
+
+export const Header = () => {
+
+  const pathname = usePathname()
+
+  return (
+    <div className={styles.container}>
+        <Link href='/'>
+            <h1>BEATRIZ&rsquo;S <span>ART</span></h1>
+        </Link>
+
+        <div className={styles.linksContainer}>
+            <Link href='/' className={ pathname === '/' ? styles.active : '' }>
+                <p>Inicio</p>
+            </Link>
+            <Link href='/about' className={ pathname === '/about' ? styles.active : '' }>
+                <p>Biografía</p>
+            </Link>
+            <Link href='/contact' className={ pathname === '/contact' ? styles.active : '' }>
+                <p>Contact</p>
+            </Link>
+        </div>
+    </div>
+  )
+}
