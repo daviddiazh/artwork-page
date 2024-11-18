f016924766261fc1460c5647d4144c6c159dc118
daviddiazh <daviddiazlds1221@gmail.com>
2024-11-17 20:54:28 -0500

    fix: new changex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,87 +1,9 @@
-'use client'
-
-import { useEffect, useState } from "react";
-import styles from "./page.module.css";
 import { artWorks } from "@/mocks/artworks";
-import { ArtWork } from "./interfaces/artwork";
-import Image from "next/image";
-import Link from "next/link";
+import { Home as HomeComponent } from './components/HomePage'
 
 export default function Home() {
 
-  const [ opt, setOpt ] = useState(0);
-  const [ data, setData ] = useState<ArtWork[]>([]);
-
-  useEffect(() => {
-    if (opt === 0) setData(artWorks)
-    if (opt === 1) setData(artWorks.filter(d => d.category === 'Abstract'))
-    if (opt === 2) setData(artWorks.filter(d => d.category === 'Landscape'))
-    if (opt === 3) setData(artWorks.filter(d => d.category === 'Portrait'))
-  }, [opt])
-
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <div className={styles.firstContainer}>
-          <p 
-            className={`
-              ${styles.firstContainerText} 
-              ${opt === 0 ? styles.active : ''}
-            `}
-            onClick={() => setOpt(0)}
-          >
-            Todos
-          </p>
-          <p 
-            className={`
-              ${styles.firstContainerText} 
-              ${opt === 1 ? styles.active : ''}
-            `}
-            onClick={() => setOpt(1)}
-          >
-            Abstracto
-          </p>
-          <p
-            className={`
-              ${styles.firstContainerText} 
-              ${opt === 2 ? styles.active : ''}
-            `}
-            onClick={() => setOpt(2)}
-          >
-            Paisajes
-          </p>
-          <p
-            className={`
-              ${styles.firstContainerText} 
-              ${opt === 3 ? styles.active : ''}
-            `}
-            onClick={() => setOpt(3)}
-          >
-            Retratos
-          </p>
-        </div>
-
-        <div className={styles.secondContainer}>
-          {
-            data?.map((item) => (
-              <Link key={item?._id} className={styles.imageContainer} href={`/detail/${item?._id}`}>
-                <div className={styles.image}>
-                  <Image
-                    src={item?.image}
-                    alt={item?.name}
-                    width={200}
-                    height={200}
-                    style={{ width: '100%', height: '100%' }}
-                  />
-                  {
-                    item?.state === 'Sold' && <p className={styles.soldText}>Vendido</p>
-                  }
-                </div>
-              </Link>
-            ))
-          }
-        </div>
-      </main>
-    </div>
+    <HomeComponent artWorks={artWorks} />
   );
 }
